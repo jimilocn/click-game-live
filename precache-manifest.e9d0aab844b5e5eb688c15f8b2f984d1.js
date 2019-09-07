348c776f885ab0f2219f13fd11578228b0e113ec
Jimi Loc Nguyen <jimilocn@gmail.com>
2019-09-07 17:21:21 -0500

    Updates

---
+++ precache-manifest.e9d0aab844b5e5eb688c15f8b2f984d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd3aad9b2d6492ca514a3e8e1ac96a3c",
+    "url": "/click-game-live/./index.html"
+  },
+  {
+    "revision": "1c442ee861b5db5cd277",
+    "url": "/click-game-live/./static/css/main.6740259e.chunk.css"
+  },
+  {
+    "revision": "606b67ae68288e2d616e",
+    "url": "/click-game-live/./static/js/2.5f124323.chunk.js"
+  },
+  {
+    "revision": "1c442ee861b5db5cd277",
+    "url": "/click-game-live/./static/js/main.68437253.chunk.js"
+  },
+  {
+    "revision": "f5fbe546cc83d7aae272",
+    "url": "/click-game-live/./static/js/runtime~main.93be06d3.js"
+  }
+]);
